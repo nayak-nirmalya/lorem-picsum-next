cbc4d2a19147dbf264c4496a11afbb2f1dd2763e
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-09 17:32:09 +0530

    added new component HeadMetadata

---
+++ components/HeadMetadata.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import Head from "next/head";
+
+interface HeadMetadataProps {
+  imageID: string;
+}
+
+export default function HeadMetadata({ imageID }: HeadMetadataProps) {
+  return (
+    <Head>
+      <meta
+        name="description"
+        content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
+      />
+      <meta
+        name="keywords"
+        content="HTML, CSS, JavaScript, TypeScript, React, Vite, TailwindCSS"
+      />
+      <meta name="author" content="Nirmalya Nayak" />
+      <title>Random Image from Lorem Picsum</title>
+
+      {/* Facebook */}
+      <meta property="og:title" content="Random Image from Lorem Picsum" />
+      <meta
+        property="og:image"
+        content={`https://picsum.photos/seed/${imageID}/1200/630`}
+      />
+      <meta property="og:image:alt" content="Random Image from Lorem Picsum" />
+      <meta
+        property="og:url"
+        content={`https://random-photo-react-picsum.vercel.app/${imageID}`}
+      />
+      <meta
+        property="og:description"
+        content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
+      />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:image"
+        content={`https://picsum.photos/seed/${imageID}/1200/630`}
+      />
+      <meta
+        property="twitter:image:alt"
+        content="Random Image from Lorem Picsum"
+      />
+      <meta name="twitter:title" content="Random Image from Lorem Picsum" />
+      <meta name="twitter:creator" content="@nirmalya_nayak" />
+      <meta
+        name="twitter:description"
+        content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
+      />
+
+      {/* WhatsApp */}
+      <meta
+        property="og:image"
+        content={`https://picsum.photos/seed/${imageID}/300/300.jpg`}
+      />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:image:width" content="300" />
+      <meta property="og:image:height" content="300" />
+    </Head>
+  );
+}
--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,5 +1,4 @@
 import { useRouter } from "next/router";
-import Head from "next/head";
 
 export default function Page() {
   const imageID = "";
@@ -8,52 +7,6 @@ export default function Page() {
     <>
       <p>Post: {router.query.id}</p>
       <div>
-        <Head>
-          {/* Facebook */}
-          <meta property="og:title" content="Random Image from Lorem Picsum" />
-          <meta
-            property="og:image"
-            content={`https://picsum.photos/seed/${imageID}/1200/630`}
-          />
-          <meta
-            property="og:image:alt"
-            content="Random Image from Lorem Picsum"
-          />
-          <meta
-            property="og:url"
-            content={`https://random-photo-react-picsum.vercel.app/${imageID}`}
-          />
-          <meta
-            property="og:description"
-            content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
-          />
-
-          {/* Twitter */}
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta
-            property="twitter:image"
-            content={`https://picsum.photos/seed/${imageID}/1200/630`}
-          />
-          <meta
-            property="twitter:image:alt"
-            content="Random Image from Lorem Picsum"
-          />
-          <meta name="twitter:title" content="Random Image from Lorem Picsum" />
-          <meta name="twitter:creator" content="@nirmalya_nayak" />
-          <meta
-            name="twitter:description"
-            content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
-          />
-
-          {/* WhatsApp */}
-          <meta
-            property="og:image"
-            content={`https://picsum.photos/seed/${imageID}/300/300.jpg`}
-          />
-          <meta property="og:image:type" content="image/jpeg" />
-          <meta property="og:image:width" content="300" />
-          <meta property="og:image:height" content="300" />
-        </Head>
         <p>Hello world!</p>
       </div>
     </>
