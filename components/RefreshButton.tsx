6cc0a546369a0153ad5acd01a75e279058bf1844
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-09 21:04:22 +0530

    refresh on btn click

--- components/RefreshButton.tsx
+++ components/RefreshButton.tsx
@@ -1,7 +1,9 @@
 export default function RefreshButton() {
   return (
     <button
-      onClick={() => window.location.reload()}
+      onClick={() =>
+        (window.location.href = "https://lorem-picsum-next.vercel.app/")
+      }
       className="relative inline-flex items-center justify-center p-0.5 mt-6 mb-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
     >
       <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,56 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
+        />
+        <meta
+          name="keywords"
+          content="HTML, CSS, JavaScript, TypeScript, React, Vite, TailwindCSS"
+        />
+        <meta name="author" content="Nirmalya Nayak" />
+
+        {/* Facebook */}
+        <meta property="og:title" content="Random Image from Lorem Picsum" />
+        <meta property="og:image" content="https://picsum.photos/1200/630" />
+        <meta
+          property="og:image:alt"
+          content="Random Image from Lorem Picsum"
+        />
+        <meta
+          property="og:url"
+          content="https://lorem-picsum-next.vercel.app/"
+        />
+        <meta
+          property="og:description"
+          content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
+        />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:image"
+          content="https://picsum.photos/1200/630"
+        />
+        <meta
+          property="twitter:image:alt"
+          content="Random Image from Lorem Picsum"
+        />
+        <meta name="twitter:title" content="Random Image from Lorem Picsum" />
+        <meta name="twitter:creator" content="@nirmalya_nayak" />
+        <meta
+          name="twitter:description"
+          content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
+        />
+
+        {/* WhatsApp */}
+        {/* <meta property="og:image" content="https://picsum.photos/300/300.jpg" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
