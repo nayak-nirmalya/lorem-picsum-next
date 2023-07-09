7af75dc0cf0bb407332bcf5b42e7fbc0101fe3d4
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-09 17:16:51 +0530

    added new component HeaderText.tsx

---
+++ components/HeaderText.tsx
@@ -0,0 +1,7 @@
+export default function HeaderText() {
+  return (
+    <h1 className="mt-24 md:mt-10 xl:mt-20 text-2xl md:text-4xl lg:text-5xl font-bold text-center">
+      Random Image from Lorem Picsum
+    </h1>
+  );
+}
--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,5 +1,17 @@
-import React from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
 
-export default function GetImageWithID() {
-  return <div>GetImageWithID</div>;
+export default function Page() {
+  const router = useRouter();
+  return (
+    <>
+      <p>Post: {router.query.id}</p>
+      <div>
+        <Head>
+          <title>My page title</title>
+        </Head>
+        <p>Hello world!</p>
+      </div>
+    </>
+  );
 }
