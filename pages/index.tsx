43faf06efded04b57d586e62d96b09daefdcf235
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-09 17:26:29 +0530

    show random img on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+import HeaderText from "@/components/HeaderText";
+import RandomImage from "@/components/RandomImage";
+import RefreshButton from "@/components/RefreshButton";
+import Share from "@/components/Share";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-center h-screen w-screen">
+      <HeaderText />
+      <RefreshButton />
+      <RandomImage />
+      <Share />
+    </div>
+  );
 }
