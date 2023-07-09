3bd6283d64e863066b5829ece31fe9dc70038cbb
Nirmalya Nayak <n.nayak263@gmail.com>
2023-07-09 20:30:27 +0530

    removed body link from headmeatadata

--- components/HeadMetadata.tsx
+++ components/HeadMetadata.tsx
@@ -7,79 +7,59 @@ interface HeadMetadataProps {
 
 export default function HeadMetadata({ imageID }: HeadMetadataProps) {
   return (
-    <>
-      <Head>
-        <meta
-          name="description"
-          content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
-        />
-        <meta
-          name="keywords"
-          content="HTML, CSS, JavaScript, TypeScript, React, Vite, TailwindCSS"
-        />
-        <meta name="author" content="Nirmalya Nayak" />
-        <title>Random Image from Lorem Picsum</title>
-
-        {/* Facebook */}
-        <meta property="og:title" content="Random Image from Lorem Picsum" />
-        <meta
-          property="og:image"
-          content={`https://picsum.photos/seed/${imageID}/1200/630`}
-        />
-        <meta
-          property="og:image:alt"
-          content="Random Image from Lorem Picsum"
-        />
-        <meta
-          property="og:url"
-          content={`https://lorem-picsum-next.vercel.app/${imageID}`}
-        />
-        <meta
-          property="og:description"
-          content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
-        />
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:image"
-          content={`https://picsum.photos/seed/${imageID}/1200/630`}
-        />
-        <meta
-          property="twitter:image:alt"
-          content="Random Image from Lorem Picsum"
-        />
-        <meta name="twitter:title" content="Random Image from Lorem Picsum" />
-        <meta name="twitter:creator" content="@nirmalya_nayak" />
-        <meta
-          name="twitter:description"
-          content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
-        />
-
-        {/* WhatsApp */}
-        <meta
-          property="og:image"
-          content={`https://picsum.photos/seed/${imageID}/300/300.jpg`}
-        />
-        <meta property="og:image:type" content="image/jpeg" />
-        <meta property="og:image:width" content="300" />
-        <meta property="og:image:height" content="300" />
-      </Head>
-
-      <link
-        itemProp="thumbnailUrl"
-        href={`https://picsum.photos/seed/${imageID}/300/300.jpg`}
+    <Head>
+      <meta
+        name="description"
+        content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
       />
-      <span
-        itemProp="thumbnail"
-        itemScope
-        itemType="http://schema.org/ImageObject"
-      >
-        <link
-          itemProp="url"
-          href={`https://picsum.photos/seed/${imageID}/300/300.jpg`}
-        />
-      </span>
-    </>
+      <meta
+        name="keywords"
+        content="HTML, CSS, JavaScript, TypeScript, React, Vite, TailwindCSS"
+      />
+      <meta name="author" content="Nirmalya Nayak" />
+      <title>Random Image from Lorem Picsum</title>
+
+      {/* Facebook */}
+      <meta property="og:title" content="Random Image from Lorem Picsum" />
+      <meta
+        property="og:image"
+        content={`https://picsum.photos/seed/${imageID}/1200/630`}
+      />
+      <meta property="og:image:alt" content="Random Image from Lorem Picsum" />
+      <meta
+        property="og:url"
+        content={`https://lorem-picsum-next.vercel.app/${imageID}`}
+      />
+      <meta
+        property="og:description"
+        content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
+      />
+
+      {/* Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:image"
+        content={`https://picsum.photos/seed/${imageID}/1200/630`}
+      />
+      <meta
+        property="twitter:image:alt"
+        content="Random Image from Lorem Picsum"
+      />
+      <meta name="twitter:title" content="Random Image from Lorem Picsum" />
+      <meta name="twitter:creator" content="@nirmalya_nayak" />
+      <meta
+        name="twitter:description"
+        content="Random Image from Lorem Picsum. Made with React, TypeScript, TailwindCSS & Vite."
+      />
+
+      {/* WhatsApp */}
+      <meta
+        property="og:image"
+        content={`https://picsum.photos/seed/${imageID}/300/300.jpg`}
+      />
+      <meta property="og:image:type" content="image/jpeg" />
+      <meta property="og:image:width" content="300" />
+      <meta property="og:image:height" content="300" />
+    </Head>
   );
 }
--- pages/[id].tsx
+++ pages/[id].tsx
@@ -8,6 +8,22 @@ export default function Page({ id }: { id: string }) {
   return (
     <div className="flex flex-col items-center h-screen w-screen">
       <HeadMetadata imageID={id} />
+
+      <link
+        itemProp="thumbnailUrl"
+        href={`https://picsum.photos/seed/${id}/300/300.jpg`}
+      />
+      <span
+        itemProp="thumbnail"
+        itemScope
+        itemType="http://schema.org/ImageObject"
+      >
+        <link
+          itemProp="url"
+          href={`https://picsum.photos/seed/${id}/300/300.jpg`}
+        />
+      </span>
+
       <HeaderText />
       <RefreshButton />
       <RandomImage imageID={id} />
